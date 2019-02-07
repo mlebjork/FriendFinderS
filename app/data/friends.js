a57ead9c8283231fa9b00da39ffb0d76a354127d
Emelie Straus <mlestraus@gmail.com>
2019-02-06 20:50:19 -0700

    working and css fixed

---
+++ app/data/friends.js
@@ -0,0 +1,117 @@
+var friends = [
+  {
+      "name": "Elaine Benes",
+      "photo": "assets/images/EB.jpg",
+      "scores": [
+          5,
+          1,
+          4,
+          4,
+          5,
+          1,
+          2,
+          5,
+          4,
+          1
+      ]
+  }
+  , {
+      "name": "George Costanza",
+      "photo": "assets/images/GC.jpg" ,  //"../../assets/images/rachel.jpg", 
+      "scores": [
+          3,
+          1,
+          5,
+          4,
+          2,
+          5,
+          1,
+          1,
+          2,
+          3
+      ]
+  },
+  {
+      "name": "Jerry Seinfeld",
+      "photo": "assets/images/JS.jpg",
+      "scores": [
+          5,
+          5,
+          5,
+          5,
+          5,
+          5,
+          5,
+          5,
+          5,
+          5
+      ]
+  },
+  {
+      "name": "Kramer",
+      "photo": "assets/images/K.jpg",
+      "scores": [
+          4,
+          4,
+          4,
+          4,
+          4,
+          4,
+          4,
+          4,
+          4,
+          4
+      ]
+  },
+  {
+      "name": "Newman",
+      "photo": "assets/images/N.jpg",
+      "scores": [
+          3,
+          3,
+          3,
+          3,
+          3,
+          3,
+          3,
+          3,
+          3,
+          3
+      ]
+  }, {
+      "name": "Susan Ross",
+      "photo": "assets/image/SR.jpg",
+      "scores": [
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+          2
+      ]
+  },
+      {
+      "name": "Soup Nazi",
+      "photo": "assets/images/SN.jpg",
+      "scores": [
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1
+      ]
+  }
+
+
+]
+
+module.exports = friends;
