1c2d32cb9ef87fa9913f27befc311344ffde6a22
Emelie Straus <mlestraus@gmail.com>
2019-02-07 13:47:16 -0700

    favicon

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
-// app.use(express.static('./app/public'))
+app.use(express.static('./app/public'))
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
